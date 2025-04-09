ef911101f746b1dffbc6835ba77bc1b3e6003629
SandeepKumarSolanki <166508938+SandeepKumarSolanki@users.noreply.github.com>
2025-04-09 13:36:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
 //your code here
 
+let arr = [2 ,1 ,2];
+
 function majorityElements(nums) {
 	let n = nums.length-1;
 	nums.sort((a,b)=> a-b);
 	return nums[Math.floor(n/2)+1];
 }
+
+console.log(majorityElements(arr));
 
