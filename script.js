ff231d2eef11c19dea426785b77c31655902d907
SandeepKumarSolanki <166508938+SandeepKumarSolanki@users.noreply.github.com>
2025-04-14 12:12:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,9 +2,9 @@
 
 let arr = [2 ,1 ,2];
 function majorElement(nums) {
+     nums.sort((a,b)=> a-b);
     let n = nums.length;
-	nums.sort((a,b)=> a-b);
-	return nums[Math.floor(n/2)+1];
+    return nums[Math.floor(n/2)];
 }
 
 
