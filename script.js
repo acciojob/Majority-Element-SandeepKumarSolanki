afd660747541813b1f4025f103f713b03710e322
SandeepKumarSolanki <166508938+SandeepKumarSolanki@users.noreply.github.com>
2025-04-14 12:05:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,35 +2,9 @@
 
 let arr = [2 ,1 ,2];
 function majorElement(nums) {
-    let candidate = null;
-    let count = 0;
-
-    // First pass: Find a potential candidate
-    for (let num of nums) {
-        if (count === 0) {
-            candidate = num;
-            count = 1;
-        } else if (num === candidate) {
-            count++;
-        } else {
-            count--;
-        }
-    }
-
-    // Second pass: Verify the candidate
-    count = 0;
-    for (let num of nums) {
-        if (num === candidate) {
-            count++;
-        }
-    }
-
-    if (count > Math.floor(nums.length / 2)) {
-        return candidate;
-    }
-
-    // In case there's no majority element (though the problem guarantees one exists)
-    return null;
+    let n = nums.length;
+	nums.sort((a,b)=> a-b);
+	return nums[Math.floor(n/2)+1];
 }
 
 
